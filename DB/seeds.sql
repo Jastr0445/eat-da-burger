0c2096369bff1b7263384bfa244e0769a41664ad
Jackson Strauss <58577918+Jastr0445@users.noreply.github.com>
2020-06-05 20:27:38 -0500

    Update seeds.sql

--- DB/seeds.sql
+++ DB/seeds.sql
@@ -1,15 +1,6 @@
--- this file is handling boolean values for burger data --
-INSERT INTO burgers (burger_name) VALUES ('cheese burger');
-INSERT INTO burgers (burger_name) VALUES ('big kahuna burger');
-INSERT INTO burgers (burger_name) VALUES ('hamburger');
-INSERT INTO burgers (burger_name) VALUES ('steak burger');
-
-
-INSERT INTO burgers (burger_name, devoured) VALUES ('cheese burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('big kahuna burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('hamburger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('steak burger', false); 
-
-
-
-
+INSERT INTO burgers (name) VALUES ('cheese burger');
+INSERT INTO burgers (name) VALUES ('big kahuna burger');
+INSERT INTO burgers (name, devoured) VALUES ('onion burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('hamburger', true);
+INSERT INTO burgers (name, devoured) VALUES ('krabby patty', true);
+INSERT INTO burgers (name) VALUES ('fajita burger');
